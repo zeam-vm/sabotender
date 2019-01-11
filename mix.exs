9108327a34805aab44402c84abcb0eeb33aee158
Susumu Yamazaki <zacky1972@gmail.com>
2019-01-11 13:27:13 +0900

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sabotender.MixProject do
   def project do
     [
       app: :sabotender,
-      version: "0.1.0",
+      version: "0.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
